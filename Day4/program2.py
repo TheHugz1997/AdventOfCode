ac0c3068ddce1040d0d77c4957fe64b32db70ecf
Hugo Dubois <195347@ecam.be>
2022-12-07 14:36:55 +0100

    Day 4

---
+++ Day4/program2.py
@@ -0,0 +1,23 @@
+import re
+with open('Day4/pairs.txt') as f:
+    lines = f.readlines()
+    liste = []
+    doublons = 0
+    for j in lines:
+        res = re.split('-|,', j.split('\n')[0])
+        liste.append(res)
+    for k in liste:
+        # premier cas a-b contient c-d
+        if int(k[0]) <= int(k[2]) and int(k[1]) >= int(k[3]):
+            doublons += 1
+
+        # deuxième cas a-b contenu dans c-d
+        elif int(k[0]) >= int(k[2]) and int(k[1]) <= int(k[3]):
+            doublons += 1
+
+        elif int(k[0]) <= int(k[2]) and int(k[1]) >= int(k[2]) and int(k[1]) <= int(k[3]):
+            doublons += 1
+        
+        elif int(k[0]) >= int(k[2]) and int(k[1]) >= int(k[3]) and int(k[3]) >= int(k[0]):
+            doublons += 1
+    print(doublons)
